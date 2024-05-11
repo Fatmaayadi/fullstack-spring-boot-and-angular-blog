edc0fea6bc03c1c936694da8727d4cd1e558a6be
Fatmaayadi <159944736+Fatmaayadi@users.noreply.github.com>
2024-05-11 23:02:48 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -11,6 +11,11 @@ pipeline {
     
     environment {
         scannerHome = tool 'SonarQubeServer'
+        NEXUS_VERSION = "nexus3"
+        NEXUS_PROTOCOL = "http"
+        NEXUS_URL = "192.168.74.134:8081"
+        NEXUS_REPOSITORY = "Mavenupload"
+        NEXUS_CREDENTIAL_ID = "nexusCredential"
     }
     
     stages {
@@ -30,6 +35,14 @@ pipeline {
             }
         }
         
+        stage('Run Frontend Unit Tests') {
+            steps {
+                dir('spring-blog-client') {
+                    // sh 'npm run test'
+                }
+            }
+        }
+
         stage('Build Frontend') {
             steps {
                 dir('spring-blog-client') {
@@ -39,25 +52,50 @@ pipeline {
             }
         }
         
-        stage('Run Frontend Unit Tests') {
-            steps {
-                dir('spring-blog-client') {
-                  //  sh 'npm run test'
-                }
-            }
-        }
-        
-         stage('Code Analysis') {
+        stage('Code Analysis') {
             steps {
                 script {
-                            sh """
-                                ${scannerHome}/bin/sonar-scanner \
-                                -Dsonar.projectKey=Test \
-                                -Dsonar.java.binaries=. \
-                                -Dsonar.host.url=http://192.168.74.139:9010 \
-                                -Dsonar.login=squ_1ca99673dccd74a3038f8b7c456368bba9b4d85b
-                            """
+                    sh """
+                        ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=Test \
+                        -Dsonar.java.binaries=. \
+                        -Dsonar.host.url=http://192.168.74.139:9010 \
+                        -Dsonar.login=squ_1ca99673dccd74a3038f8b7c456368bba9b4d85b
+                    """
                 }
             }
         }
         
+        stage('Deploy Artifacts to Nexus') {
+            steps {
+                script {
+                    pom = readMavenPom file: "pom.xml";
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path;
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: ARTIFACT_VERSION,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging]
+                            ]
+                        );
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
+                }
+            }
+        }
+    }
+}
