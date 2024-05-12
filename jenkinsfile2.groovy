cfdc934c42ff24b6020879c6698d8e9632d6d897
Fatmaayadi <159944736+Fatmaayadi@users.noreply.github.com>
2024-05-12 22:40:16 +0100

    Create jenkinsfile2.groovy

---
+++ jenkinsfile2.groovy
@@ -0,0 +1,59 @@
+pipeline {
+    agent any
+    
+    tools {
+        nodejs 'NodeJS'
+        jdk 'Java'
+    } 
+    
+    environment {
+        scannerHome = tool 'SonarQubeServer'
+    }
+    
+    stages {
+        stage('SonarQube Code Analysis') {
+            steps {
+                script {
+                    dir('spring-blog-client') {
+                        sh 'npm install --legacy-peer-deps'
+                        sh 'npm run build -- --configuration=production'
+                        sh """
+                            ${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=Test \
+                            -Dsonar.java.binaries=. \
+                            -Dsonar.host.url=http://192.168.74.139:9010 \
+                            -Dsonar.login=squ_1ca99673dccd74a3038f8b7c456368bba9b4d85b
+                        """
+                    }
+                }
+            }
+        }
+        
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    dir('spring-blog-client') {
+                        bat 'docker build -t fatma24/frontend:latest .'
+                    }
+                }
+            }
+        }
+        
+        stage('Push to Docker Hub') {
+            steps {
+                script {
+                        bat 'docker login -u $fatma24 -p $rootroot24'
+                        bat 'docker push fatma24/frontend:latest'
+                    }
+                }
+            }
+        
+        stage('Create Container') {
+            steps {
+                script {
+                    bat 'docker run -d --name frontend fatma24/frontend:latest'
+                }
+            }
+        }
+    }
+}
