5d8c2c353ae1ffc351312c57c544ae691c22706a
Fatmaayadi <159944736+Fatmaayadi@users.noreply.github.com>
2024-05-12 01:00:04 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -63,23 +63,25 @@ pipeline {
         
         stage('Deploy Nexus') {
             steps {
-                nexusArtifactUploader(
-                    artifacts: [
-                        [
-                            artifactId: 'spring-blog-backend',
-                            classifier: '',
-                            file: 'target/spring-blog-backend-0.0.1-SNAPSHOT.jar',
-                            type: 'jar'
-                        ]
-                    ], 
-                    credentialsId: 'nexusCredential',
-                    groupId: 'com.github.braians',
-                    nexusUrl: '192.168.74.134:8081',
-                    nexusVersion: 'nexus3',
-                    protocol: 'http',
-                    repository: 'Mavenupload',
-                    version: '0.0.1'
-                )
+                dir('spring-blog-backend') {
+                    nexusArtifactUploader(
+                        artifacts: [
+                            [
+                                artifactId: 'spring-blog-backend',
+                                classifier: '',
+                                file: 'target/spring-blog-backend-0.0.1-SNAPSHOT.jar',
+                                type: 'jar'
+                            ]
+                        ], 
+                        credentialsId: 'nexusCredential',
+                        groupId: 'com.github.braians',
+                        nexusUrl: '192.168.74.134:8081',
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        repository: 'Mavenupload',
+                        version: '0.0.1'
+                    )
+                }
             }
         }
     }
